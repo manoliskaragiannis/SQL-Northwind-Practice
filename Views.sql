67079c9720fe0f82f6c18befc46d5511e650017f
manoliskaragiannis <manolis.karagianniss@gmail.com>
2026-05-19 19:57:15 +0300

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -29,3 +29,33 @@ SELECT
 FROM Products AS P
 JOIN Suppliers AS S ON P.SupplierID = S.SupplierID
 WHERE P.UnitsInStock <= P.ReorderLevel;
+CREATE VIEW vw_ProductDetails AS
+SELECT 
+    P.ProductID, 
+    P.ProductName, 
+    C.CategoryName,
+    S.CompanyName AS SupplierName
+FROM Products AS P
+JOIN Categories AS C ON P.CategoryID = C.CategoryID
+JOIN Suppliers AS S ON P.SupplierID = S.SupplierID;
+GO
+
+CREATE VIEW cw_CustomerOrders AS
+SELECT
+    C.CustomerID,
+    C.CompanyName,
+    O.OrderID
+    FROM Customers AS C
+    JOIN Orders AS O ON C.CustomerID = O.CustomerID
+GO
+
+CREATE VIEW vw_UrgentOrders AS
+SELECT 
+    OrderID,
+    CustomerID,
+    OrderDate,
+    RequiredDate,
+    ShippedDate
+FROM Orders
+WHERE  ShippedDate IS NULL AND RequiredDate < '1997-09-30'
+GO
